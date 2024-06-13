67806c22f529ff22cf76231b7c08ada8d8334b88
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-13 10:01:08 -0700

    updated getBookByTitle()

--- src/main/java/com/example/e_commerce_api/controller/BookController.java
+++ src/main/java/com/example/e_commerce_api/controller/BookController.java
@@ -46,17 +46,17 @@ public class BookController {
     }
 
     @GetMapping("/books/title/{title}")
-    public Book getBookByTitle(@PathVariable String title) {
+    public ResponseEntity<?> getBookByTitle(@PathVariable String title) {
         logger.info("Getting book by title: {}", title);
 
         Book result = service.getBookByTitle(title);
         if (result == null) {
             logger.warn("Book not found");
-            return null;
+            return ResponseEntity.status(404).body("Book not found");
         }
 
         logger.info("Book successfully found");
-        return service.getBookByTitle(title);
+        return ResponseEntity.ok().body(result);
     }
 
     @PostMapping("/books")
