c9a5be1da6f4406e3875594045440f6bfe073f27
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-13 08:45:38 -0700

    merge

--- src/main/java/com/example/e_commerce_api/controller/BookController.java
+++ src/main/java/com/example/e_commerce_api/controller/BookController.java
@@ -2,6 +2,9 @@ package com.example.e_commerce_api.controller;
 
 import com.example.e_commerce_api.entity.Book;
 import com.example.e_commerce_api.service.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,45 +12,65 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class BookController {
 
     @Autowired
     BookService service;
 
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
+
     @GetMapping("/books")
     public List<Book> getAllBooks() {
+        logger.info("All books are retrieved.");
         return service.getAllBooks();
     }
 
     @GetMapping("/books/sorted")
     public List<Book> getAllBooksByTitleOrder() {
+        logger.info("All books are retrieved by title order.");
+
         return service.getAllBooksByTitleOrder();
     }
 
     @GetMapping("/books/id/{id}")
     public ResponseEntity<?> getBookById(@PathVariable long id) {
+
+
         Book result = service.getBookById(id);
-        if (result == null)
+        if (result == null) {
+            logger.warn("Book is not found");
             return ResponseEntity.status(404).body("Book not found");
+        }
+        logger.info("A book is  retrieved by Id.");
         return ResponseEntity.ok().body(result);
     }
 
     @GetMapping("/books/title/{title}")
     public Book getBookByTitle(@PathVariable String title) {
+        logger.info("A book is retrieved by title");
+
+
         return service.getBookByTitle(title);
     }
 
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
 
+
         // Prevent overwriting a book with same ID
         book.setId(0);
 
         // Check if book with given title already exists
         Book result = service.getBookByTitle(book.getTitle());
-        if (result != null)
-            return null;
 
+
+        if (result != null) {
+            logger.warn("Book already exists");
+            return null;
+        }
+        logger.info("Book is added to the storage.");
         return service.addBook(book);
     }
 
@@ -56,8 +79,13 @@ public class BookController {
 
         // Check if update target exists
         Book result = service.getBookById(book.getId());
-        if (result == null)
+
+        if (result == null) {
+            logger.warn("Book does not exist.");
             return null;
+        }
+
+        logger.info("Book is updated.");
 
         return service.updateBook(book);
     }
@@ -67,14 +95,20 @@ public class BookController {
 
         Book result = service.getBookById(id);
 
-        // Check if book exists and if it's in stock
-        if (result == null || result.getQuantity() <= 0)
-            return false;
 
+        // Check if book exists and if it's in stock
+        if (result == null || result.getQuantity() <= 0) {
+            logger.warn("Book is not in stock.");
+
+            return false;
+        }
         // Record purchase by decrementing quantity by 1
         result.setQuantity(result.getQuantity() - 1);
-        if (service.updateBook(result) != null)
+        if (service.updateBook(result) != null) {
+            logger.info("Book has been purchased");
             return true;
+        }
+        logger.warn("Book has not been purchased");
         return false;
     }
 
@@ -85,8 +119,11 @@ public class BookController {
         service.deleteBookById(id);
 
         // If count changed, then delete was successful
-        if (count != service.getAllBooks().size())
+        if (count != service.getAllBooks().size()) {
+            logger.info("Book has been deleted");
             return true;
+        }
+        logger.warn("Book has not been deleted");
         return false;
     }
 
